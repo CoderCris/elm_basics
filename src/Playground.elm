b3dc16669bb7a5874e00599c2922b59aba040625
CoderCris <crisamer.ortega@gmail.com>
2024-07-23 18:15:21 +0000

    playing with views

---
+++ src/Playground.elm
@@ -0,0 +1,32 @@
+module Playground exposing (main)
+
+import Html
+
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+
+        "Monday" ->
+            "#MondayBlues"
+
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#FlashbackFriday"
+
+        "Saturday" ->
+            "#Caturday"
+
+        _ ->
+            "#Whatever"
+
+main =
+    Html.text <| hashtag 5
